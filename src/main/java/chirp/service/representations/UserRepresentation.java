6f449ce6612f1ea48e24a3e9a9997c08a6dc0983
Dan Rosen <dan@marakana.com>
2013-02-26 16:56:41 -0800

    added self link to UserRepresentation

--- src/main/java/chirp/service/representations/UserRepresentation.java
+++ src/main/java/chirp/service/representations/UserRepresentation.java
@@ -1,26 +1,35 @@
 package chirp.service.representations;
 
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import chirp.model.User;
+import chirp.service.resources.UserResource;
 
 public class UserRepresentation {
 
 	private final String username;
 	private final String realname;
+	private final URI self;
 
 	public UserRepresentation(User user) {
 		this.username = user.getUsername();
 		this.realname = user.getRealname();
+		self = UriBuilder.fromResource(UserResource.class).path(username).build();
 	}
 
 	@JsonCreator
 	public UserRepresentation(
 			@JsonProperty("username") String username,
-			@JsonProperty("realname") String realname) {
+			@JsonProperty("realname") String realname,
+			@JsonProperty("self") URI self) {
 		this.username = username;
 		this.realname = realname;
+		this.self = self;
 	}
 
 	public String getUsername() {
@@ -31,4 +40,7 @@ public class UserRepresentation {
 		return realname;
 	}
 
+	public URI getSelf() {
+		return self;
+	}
 }
